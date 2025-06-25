9da778a3183aef48040109906c8a34e584230b55
twagner-99 <wagnertaylor1993@gmail.com>
2025-06-25 14:40:22 -0700

    Add to-do notes.

--- javascript.js
+++ javascript.js
@@ -16,10 +16,30 @@ function Players(name, marker) {
     }
 }
 
-const player1 = Players('Taylor', 'X'); // Can create these in console later or in code.
-const player2 = Players('Alex', 'O');
+const player1 = Players('Taylor', 'X'); // Can create these in console later. Can be created within another function.
+const player2 = Players('Alex', 'O');   // Then can get the names from user input when UI is added.
+
+function gameController(index) {
+    
+}
 
 // Things needed:
     // gameboard
     // players
     // something to control the game
+
+// To-do
+    // function to create players - DONE
+    // function to create gameboard - DONE
+    // function to play game
+        // pass function an index and mark it.
+            // Extend that function to alternate between X (player1) and O (player2)
+                // Use : ? 
+        // if there's something in that spot, don't overwrite and make player choose again
+        // functionality to tell if there's a winner
+            // Array layout and indices:
+                // 0, 1, 2
+                // 3, 4, 5
+                // 6, 7, 8
+            // I can hard code these win conditiions... or check my array methods maybe there's something better
+    // Could everything go into the gameplay function as an IIFE?
