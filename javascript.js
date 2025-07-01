8b68f6f6803039e2b500ccef62af1e2459ecbc65
twagner-99 <wagnertaylor1993@gmail.com>
2025-07-01 16:05:48 -0700

    Add functions to play a round.
    Gives ref errors, need to fix where certain
    functions are defined.

--- javascript.js
+++ javascript.js
@@ -26,50 +26,70 @@ function playerController() { // I don't think this can be an IIFE if I want to 
         }
     }
 
-    let activePlayer = player1;
+    let activePlayer = player1;         // all this stuff should be in gamecontroller?
+    // const setPlayer1 = () => {
+    //     let activePlayer = player1;
+    //     return activePlayer;
+    // }
+    
+    const getActivePlayer = () => activePlayer;
     
     // each time a marker is placed, toggle active player
-    
     const toggleActivePlayer = () => {
         activePlayer = (activePlayer === player1) ? player2 : player1;
     }
 
-    const getActivePlayer = () => activePlayer;
 
     return {
         createPlayer,
-        toggleActivePlayer,
+        // setPlayer1,
         getActivePlayer,
+        toggleActivePlayer,
     }
 
 }
 
 
 function gameController() {
-    let board = gameboardController();
+    const gameboard = gameboardController();
     const players = playerController(); // could skip by changing playercontroller to function expression called players.
-    
+
+
     const player1 = players.createPlayer('Taylor', 'X'); // Can create these in console later. Can be created within another function.
     const player2 = players.createPlayer('Alex', 'O');   // Then can get the names from user input when UI is added.
+    console.log(player1);
 
+    // will need function to createPlayer from userinput when we add UI
+    // actually, can prob just add a console prompt. Like function playerInput
 
+    const placeMarker = (index) => { // this seems liek it should be it's own thing outside of gamecontroller
+        let board = gameboard.getBoard(); // Do I need to do this to utilize updateBoard? // board is already in gameController which I ran so I'm not sure
+        let activePlayer = players.getActivePlayer();
 
-    function placeMarker() {
+        console.log(`${activePlayer.name}'s turn`);
+
+        gameboard.updateBoard(index, activePlayer.marker);
+        
+        players.toggleActivePlayer;
         
         // get the board
+        // set player1
         // get active player
         // tell active player it's their turn
-        // if player1, X. if player2, O.
-        // update board
+        // update board. if player1, X. if player2, O.
         // toggle active player
     }
 
-
-    // function updateBoard(index) {
-    //     get
-    // }
+    return {
+        placeMarker,
+    }
 }
 
+
+
+// to play, gameController().placeMarker(index)
+// or can initialize by doing gameController...
+    // or, make it an IIFE then we can just do gameController.placeMarker(index);
 
 
 
