934fd9828af6793e7dd8e4aa54adc5eb283a58b0
twagner-99 <wagnertaylor1993@gmail.com>
2025-08-06 15:41:45 -0700

    Implement win conditions for diagonal.

--- javascript.js
+++ javascript.js
@@ -16,15 +16,18 @@ const gameboardController = (function() {
         board[row][column] = marker;
     }
 
-    const winChecker = () => {
+    const winChecker = () => {  // I defined this here b/c that's where the rows and columns are defined. If I put it under gameController, I would need to re-access the rows and columns variables or read the board again to get that info. Felt cleaner to do it this way.
         // DOWN
+            // [0][0] & [1][0] & [2][0]
+            // [0][1] & [1][1] & [2][1]
+            // [0][2] & [1][2] & [2][2]
         for (let j = 0; j < columns; j++) {
             let markerCount = 0;                    // Can repeat these variable names b/c of block scope, but not sure if it's good practice. 
             let currentMarker = board[0][j];        // May be better to delineate (like markerCountDown) for debugging purposes...
             for (let i = 0; i < rows; i++) {
-                if (!!currentMarker && currentMarker === board[i][j]) {               // If value at index is empty string, do stuff
-                    currentMarker = board[i][j];
-                    markerCount++;
+                if (!!currentMarker && currentMarker === board[i][j]) { // If current spot is not an empty string, DO STUFF. I THINK this works this way b/c if statements converT to boolean.
+                    currentMarker = board[i][j];                        // !!'' evaluates to false. So if we have an empty string and we have !!'' evaluating to false
+                    markerCount++;                                      // then the if statment says, "yes it's true that !!'' is false" so it runs the code with &&
 
                     if (markerCount === rows) {
                         return true;
@@ -38,12 +41,15 @@ const gameboardController = (function() {
         }
 
         // ACROSS
+            // [0][0] & [0][1] & [0][2]
+            // [1][0] & [1][1] & [1][2]
+            // [2][0] & [2][1] & [2][2]
         for (let i = 0; i < rows; i++) {
             let markerCount = 0;
             let currentMarker = board[i][0];
             for (let j = 0; j < columns; j++) {
-                if (!!currentMarker && currentMarker === board[i][j]) {               // If value at index is empty string, do stuff
-                    currentMarker = board[i][j];
+                if (!!currentMarker && currentMarker === board[i][j]) { // change to j + 1 to avoid comparing against self? Would need to update markerCount formula.
+                    currentMarker = board[i][j];                        // Not sure if one is preferred over the other... the j+1 version might be more clear
                     markerCount++;
 
                     if (markerCount === columns) {
@@ -54,19 +60,46 @@ const gameboardController = (function() {
                 else {
                     break;
                 }
-
             }
         }
 
-        // DIAG
-        // for (let i = 0; i < rows; i++) {
-        //     for (let j = 0; j < columns; j++) {
-        //         board[i + 1][j]
-        //     }
+        // DIAGONAL-1
+            // [0][0] & [1][1] & [2][2]
+        let markerCountDiag1 = 0;
+        for (let i = 0, j = 0; i < (rows - 1); i++, j++) {
+            let currentMarker = board[i][j];
+            if (!!currentMarker && currentMarker === board[i + 1][j + 1]) {
+                currentMarker = board[i + 1][j + 1];
+                markerCountDiag1++;
 
-        // forEach
-        //     let i = 0
-        // }
+                if (markerCountDiag1 === (rows - 1)) {
+                    return true;
+                }
+            }
+            
+            else {
+                break;
+            }
+        }
+
+        // DIAGONAL-2
+            // [2][0] & [1][1] & [0][2]
+        let markerCountDiag2 = 0;
+        for (let i = 2, j = 0; j < (columns - 1); i--, j++) {
+            let currentMarker = board[i][j];
+            if (!!currentMarker && currentMarker === board[i - 1][j + 1]) {
+                currentMarker = board[i - 1][j + 1];
+                markerCountDiag2++;
+
+                if (markerCountDiag2 === (rows - 1)) {
+                    return true;
+                }
+            }
+            
+            else {
+                break;
+            }
+        }
     }
 
     return {
@@ -122,13 +155,13 @@ const gameController = (function() {
         placeMarker(row, column);
         gameboardController.winChecker();
 
-        if (!gameboardController.winChecker()) {
-            playerController.toggleActivePlayer();
-            console.log(`${playerController.getActivePlayer().name}'s turn`);
-        }
-
-        else {
+        if (gameboardController.winChecker()) {
             console.log(`${playerController.getActivePlayer().name} wins!`);
+        }
+        
+        else {
+            playerController.toggleActivePlayer();  // need to change so it doesn't toggle if player goes in occupied space
+            console.log(`${playerController.getActivePlayer().name}'s turn`);
         }
     }
 
@@ -138,97 +171,4 @@ const gameController = (function() {
 
 })();
 
-// to play, gameController.placeMarker(row, column)
-// gameController.playRound(row, column)
-
-// create playRound function that places marker, creates player, toggles, etc?
-
-
-
-
-
-
-// Things needed:
-    // gameboard
-    // players
-    // something to control the game
-
-// To-do
-    // function to create players - DONE
-    // function to create gameboard - DONE
-    // function to play game
-        // pass function an index and mark it.
-            // Extend that function to alternate between X (player1) and O (player2)
-                // Use : ? 
-        // if there's something in that spot, don't overwrite and make player choose again
-        // functionality to tell if there's a winner
-            // Array layout and indices:
-                // 0, 1, 2
-                // 3, 4, 5
-                // 6, 7, 8
-            // I can hard code these win conditiions... or check my array methods maybe there's something better
-    
-    // // 1D ARRAY BRAINSTORM
-    // let board = [0, 1, 2,
-    //              3, 4, 5, 
-    //              6, 7, 8]
-    
-    // // DOWN
-    //     // 0, 1, 2 plus three = win
-    // // ACROSS
-    //     // 0, 3, 6 plus one = win
-    // // DIAG
-    //     // 0 plus four = win
-    //     // 2 plus two = win
-
-    // // 2D ARRAY BRAINSTORM
-    //     let board = [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
-
-    //     let board = [
-    //         [0, 1, 2],
-    //         [0, 1, 2],
-    //         [0, 1, 2]
-    //     ]
-
-    //     let test = board[0][0]
-    //     board.forEach((item) => {
-    //         board.forEach((item) => {
-    //             if test === X or O
-    //             let test === item
-    //         })
-
-    //         return winner
-    //     })
-
-    //     // DOWN
-    //     for (let i = 0; i < ; i++)
-    //         for (let j = 0; j < ; j++)
-    //             board[j][i]
-
-    //     // ACROSS
-    //     for (let i = 0; i < ; i++)
-    //         for (let j = 0; j < ; j++)
-    //             board[i][j]
-
-    //     // DIAG
-    //     for (let i = 0; i < ; i++)
-    //         for (let j = 0; j < ; j++)
-    //             board[i + 1][j]
-
-    //     forEach
-    //         let i = 0
-
-
-    // // DOWN, EVERY LAST IS SAME
-    //     [0][0] & [1][0] & [2][0]
-    //     [0][1] & [1][1] & [2][1]
-    //     [0][2] & [1][2] & [2][2]
-
-    // // ACROSS, EVERY FIRST IS SAME
-    //     [0][0] & [0][1] & [0][2]
-    //     [1][0] & [1][1] & [1][2]
-    //     [2][0] & [2][1] & [2][2]
-
-    // // DIAGONAL
-    //     [0][0] & [1][1] & [2][2]
-    //     [2][0] & [1][1] & [0][2]
+// to play, gameController.playRound(row, column)
